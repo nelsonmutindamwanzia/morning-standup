5fa46ad93ef73f62ad3daa073332ab7d85beacc7
Jaffar Hussein <jaffhussein@icloud.com>
2022-08-22 15:53:20 +0200

    Fix bug in appendChild of p

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
     let form = document.querySelector('form')
     form.addEventListener('submit', (e) => {
         e.preventDefault() // prevents the form from doing its *default*
-        buildToDo (e.target.new_todo.value)
+        buildToDo(e.target.new_todo.value)
         form.reset() //resets textfield after to do event is submitted
     })
 });
-    
-function buildToDo(todo){
+
+function buildToDo(todo) {
     let p = document.createElement('p') //creates paragraph
     let btn = document.createElement('button') //creates a button 
-    
+
     btn.addEventListener('click', handleDelete)
     btn.textContent = 'x'
     p.textContent = `${todo} `; //interpolation used to create space between event and button
@@ -19,13 +19,13 @@ function buildToDo(todo){
     document.querySelector('#todo_container').appendChild(p)
 }
 
-function handleDelete(e){
+function handleDelete(e) {
     e.target.parentNode.remove() // give the btn functionality to delete the to do event
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log ("After DOM Loaded")
+    console.log("After DOM Loaded")
 })
 
-    console.log("Before DOM Loads")
+console.log("Before DOM Loads")
